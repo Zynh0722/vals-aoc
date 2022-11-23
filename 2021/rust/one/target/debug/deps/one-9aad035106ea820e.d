b1af716bd1f4cf8a23f774d2e5fb036f53676abc
Zynh0722 <Zynh0722@gmail.com>
2022-11-22 19:52:14 -0800

    remove target, whoops

--- 2021/rust/one/target/debug/deps/one-9aad035106ea820e.d
+++
@@ -1,5 +0,0 @@
-/Users/zynh/Documents/projects/vals-advent-of-code/2021/rust/one/target/debug/deps/one-9aad035106ea820e: src/main.rs
-
-/Users/zynh/Documents/projects/vals-advent-of-code/2021/rust/one/target/debug/deps/one-9aad035106ea820e.d: src/main.rs
-
-src/main.rs:
--- 2021/rust/one/target/debug/one.d
+++
@@ -1,1 +0,0 @@
-/Users/zynh/Documents/projects/vals-advent-of-code/2021/rust/one/target/debug/one: /Users/zynh/Documents/projects/vals-advent-of-code/2021/rust/one/src/main.rs
